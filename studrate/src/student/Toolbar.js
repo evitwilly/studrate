c8d819cdeabd5532ff4ef39a849e79bbc1489bf1
evitwilly <dmitry.tehnicov@gmail.com>
2022-04-17 13:49:33 +0300

    убрал кнопки и чекбоксы для тестов

--- studrate/src/student/Toolbar.js
+++ studrate/src/student/Toolbar.js
@@ -20,47 +20,6 @@ export default class Toolbar extends React.Component {
 		return <div className="toolbar_box">
 			<h2 className="toolbar_title">Студенты</h2>
 			<div className="toolbar_buttons">
-				<div className="test_label">Вторая группа: <input checked={this.state.isTwoGroup} onChange={() => {
-					this.setState({ isTwoGroup: !this.state.isTwoGroup });
-				}} className="test_checkbox" type="checkbox" /></div>
-			
-				<div className="test_label">Третья группа: <input checked={this.state.isThreeGroup} onChange={() => {
-					this.setState({ isThreeGroup: !this.state.isThreeGroup });
-				}} className="test_checkbox" type="checkbox" /></div>
-				
-				<input className="test_input margin_right_8" placeholder="группы" onChange={(event) => {
-					this.setState({groupCount: event.target.value});
-				}} value={this.state.groupCount} />
-
-				<input className="test_input" placeholder="студенты" onChange={(event) => {
-					this.setState({studentCount: event.target.value});
-				}} value={this.state.studentCount} />
-
-				<div className="toolbar_button noselect" onClick={() => {
-					const groupInteger = parseInt(this.state.groupCount);
-					const studentInteger = parseInt(this.state.studentCount);
-
-					if (!isNaN(groupInteger) && !isNaN(studentInteger)) {
-						axios.post(constants.restData.postStudentGenerate, {
-							groupCount: groupInteger,
-							studentCount: studentInteger,
-							isTwoGroup: this.state.isTwoGroup,
-							isThreeGroup: this.state.isThreeGroup
-						}).then(response => {
-							if (response.data["status"] == "success") {
-								this.props.update();
-							}
-						});	
-					}
-				}}>Сгенерировать</div>
-				<div className="toolbar_button noselect" onClick={() => {
-					axios.post(constants.restData.postStudentClear).then(response => {
-						if (response.data["status"] == "success") {
-							this.props.update();
-						}
-					});
-				}}>Удалить</div>
-
 				<label className="toolbar_button noselect" onChange={(event) => {
 
 					this.props.onStartImporting();
@@ -77,7 +36,6 @@ export default class Toolbar extends React.Component {
 					});
 					
 				}}><input type="file" />Импорт</label>
-
 			</div>
 		</div>;
 	}
